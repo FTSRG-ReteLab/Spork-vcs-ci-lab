594ba3006a3be47add3411ab3bb893db17a24bdc
marcsello <punkosdmarcell@rocketmail.com>
2019-04-01 06:50:49 -0700

    Fixed bug with interrupting

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -43,6 +43,7 @@ public class TrainControllerImpl extends Thread implements TrainController {
 			}
 		} catch (InterruptedException e) {
 			System.out.println("rip");
+			Thread.currentThread().interrupt();
 		}
 	}
 
